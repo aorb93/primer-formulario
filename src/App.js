6557887dffb0b45e5cb616aa038675720b9fc29e
aorb93 <aorb93@gmail.com>
2020-10-19 13:14:57 -0700

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+
+class Formulario extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  syncChanges(value, property) {
+    let state = {};
+    state[property] = value;
+    this.setState(state);
+  }
+
+  submitForm = () => {
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          onChange={ev => {
+            this.syncChanges(ev.target.value, "email");
+          }}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={ev => {
+            this.syncChanges(ev.target.value, "password");
+          }}
+          type="password"
+          placeholder="*****"
+        />
+        <div>
+          <input
+            onClick={this.submitForm}
+            type="submit"
+            value="Iniciar Sesión"
+          />
+        </div>
+      </form>
+    );
+  }
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Formulario />
     </div>
   );
 }
